37938db6839030b721fd342b820e9322f3538202
ating093 <ating093@yahoo.com.hk>
2014-06-26 18:41:09 +0800

    Update jQ.js

--- js/jQ.js
+++ js/jQ.js
@@ -1,32 +1,30 @@
-function ($) {
-    "use strict";
-    var slideshow = (function () {
-        var counter = 0,
-            i,
-            j,
-            slides =  $("#slideshow .slide"),
-            slidesLen = slides.length - 1;
-        for (i = 0, j = 9999; i &lt; slides.length; i += 1, j -= 1) {
-            $(slides[i]).css("z-index", j);
+"use strict";
+var slideshow = (function () {
+    var counter = 0,
+        i,
+        j,
+        slides =  $("#slideshow .slide"),
+        slidesLen = slides.length - 1;
+    for (i = 0, j = 9999; i &lt; slides.length; i += 1, j -= 1) {
+        $(slides[i]).css("z-index", j);
+    }
+    return {
+        startSlideshow: function () {
+            window.setInterval(function () {
+                if (counter === 0) {
+                    slides.eq(counter).fadeOut();
+                    counter += 1;
+                } else if (counter === slidesLen) {
+                    counter = 0;
+                    slides.eq(counter).fadeIn(function () {
+                        slides.fadeIn();
+                    });
+                } else {
+                    slides.eq(counter).fadeOut();
+                    counter += 1;
+                }
+            }, 2000);
         }
-        return {
-            startSlideshow: function () {
-                window.setInterval(function () {
-                    if (counter === 0) {
-                        slides.eq(counter).fadeOut();
-                        counter += 1;
-                    } else if (counter === slidesLen) {
-                        counter = 0;
-                        slides.eq(counter).fadeIn(function () {
-                            slides.fadeIn();
-                        });
-                    } else {
-                        slides.eq(counter).fadeOut();
-                        counter += 1;
-                    }
-                }, 2000);
-            }
-        };
-    }());
-    slideshow.startSlideshow();
-}
+    };
+}());
+slideshow.startSlideshow();
